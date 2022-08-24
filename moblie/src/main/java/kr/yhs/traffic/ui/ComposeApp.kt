0683c6d8bb5393a71c4ff52600a6daca054200a7
gunyu1019 <gunyu1019@gmail.com>
2022-08-24 15:50:03 +0900

    add title, favorite station

--- moblie/src/main/java/kr/yhs/traffic/ui/ComposeApp.kt
+++ moblie/src/main/java/kr/yhs/traffic/ui/ComposeApp.kt
@@ -1,41 +1,40 @@
 package kr.yhs.traffic.ui
 
 import android.app.Activity
-import android.hardware.biometrics.BiometricManager
-import android.provider.Settings.Global.getString
+import android.graphics.Paint
+import android.text.style.StyleSpan
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.interaction.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.booleanResource
-import androidx.compose.ui.semantics.dismiss
-import androidx.compose.ui.semantics.paneTitle
-import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.core.content.res.TypedArrayUtils.getString
+import androidx.compose.ui.unit.sp
 import de.charlex.compose.BottomDrawerScaffold
 import de.charlex.compose.rememberBottomDrawerScaffoldState
-import kotlinx.coroutines.launch
+import kr.yhs.traffic.models.ArrivalInfo
+import kr.yhs.traffic.models.StationRoute
 import kr.yhs.traffic.ui.component.SearchBox
 import kr.yhs.traffic.ui.theme.AppTheme
-import kotlin.math.max
-import kotlin.math.roundToInt
+import kr.yhs.traffic.ui.component.FavoriteArrival
 
 
 @OptIn(ExperimentalMaterialApi::class, ExperimentalComposeUiApi::class)
@@ -79,8 +78,109 @@ fun ComposeApp(activity: Activity? = null) {
         gesturesEnabled = bottomDrawerGestureEnableStatus,
         scaffoldState = bottomDrawerScaffoldState
     ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(30.dp)
+        ) {
+            // Title View
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                TitleText(listOf("1154번", "버스가"), listOf(MaterialTheme.colors.primary))
+                Spacer(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxWidth())
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Icon(Icons.Rounded.Settings, "setting icon")
+                }
+            }
+            TitleText(listOf("잠시 후", "도착 예정"), listOf(MaterialTheme.colors.primary))
+            Spacer(Modifier.height(30.dp))
 
+            // Favorite Bus Station
+            val listState = rememberLazyListState()
+            CardTitleText(title = "즐겨찾는 정류장")
+
+            LazyRow(
+                state = listState,
+                modifier = Modifier.padding(top = 10.dp)
+            ) {
+                item {
+                    FavoriteArrival(
+                        busInfo = StationRoute(
+                            "1001",
+                            "3000",
+                            1001,
+                            isEnd = false,
+                            isWait = false,
+                            arrivalInfo = listOf(
+                                ArrivalInfo(
+                                    carNumber = "12가4567",
+                                    congestion = null,
+                                    isArrival = false,
+                                    isFull = true,
+                                    lowBus = true,
+                                    prevCount = 1,
+                                    seat = 1,
+                                    time = 60
+                                ), ArrivalInfo(
+                                    carNumber = "89가4567",
+                                    congestion = null,
+                                    isArrival = false,
+                                    isFull = true,
+                                    lowBus = true,
+                                    prevCount = 1,
+                                    seat = 3,
+                                    time = 360
+                                )
+                            )
+                        )
+                    )
+                }
+            }
+            // TODO(INSERT SOURCE CODE)
+            Spacer(Modifier.height(30.dp))
+
+            // Around Bus Station
+            // TODO(INSERT SOURCE CODE)
+            Spacer(Modifier.height(30.dp))
+        }
     }
+}
+
+
+@Composable
+fun TitleText(
+    sentence: List<String>,
+    textColors: List<Color> = listOf()
+) {
+    Text(
+        buildAnnotatedString {
+            sentence.withIndex().forEach { (index, text) ->
+                this@buildAnnotatedString.withStyle(
+                    style = SpanStyle(color = textColors.getOrNull(index)?:Color.Unspecified)
+                ) {
+                    append("$text ")
+                }
+            }
+        }, fontSize = 40.sp,
+        fontWeight = FontWeight.Black
+    )
+}
+
+
+@Composable
+fun CardTitleText(title: String) {
+    Text(
+        text = title,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    )
 }
 
 
